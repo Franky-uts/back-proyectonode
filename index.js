b2ff892ed36a4443d258be43f7dc725e284b3b30
Franky-uts <107886319+Franky-uts@users.noreply.github.com>
2022-10-19 12:36:26 -0500

    zy

--- index.js
+++ index.js
@@ -12,16 +12,30 @@ app.use(bodyparser.urlencoded({
 }))
 app.use(bodyparser.json())
 //conexion a la base de datos 
+const uri = `mongodb+srv://${process.env.username}:${process.env.password}@cluster0.cc0lwjh.mongodb.net/${process.env.dbname = Practica
+}?retryWrites=true&w=majority`
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Conectado a BD')
+}).catch(e => {
+    console.error('error: ',e)
+} )
 
 //importar Rutas
+const authRoutes = require('./routes/auth')
 
 //Ruta del Middleware
+app.use('./api/user',authRoutes)
+
+
 app.get('/',(rep,res) => {
     res.json ({
         estado:true,
         mensaje: 'works fine!!!'
     })
-} )
+})
 //Iniciar servidor
 const PORT = process.env.PORT||3001
 app.listen(PORT, () =>{
